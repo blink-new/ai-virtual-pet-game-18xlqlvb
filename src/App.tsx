f72e94a77f312921b16ec8326f931f7dfbfa9c18
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-04-28 18:15:11 +0000

    Fix Path Alias and Import Issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import { PetDisplay } from './components/PetDisplay'
 import { Button } from './components/ui/button'
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Toaster position="top-right" />
-    <App />
-  </React.StrictMode>,
-) 
+// Make sure we have a valid DOM element before rendering
+const rootElement = document.getElementById('root')
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <Toaster position="top-right" />
+      <App />
+    </React.StrictMode>,
+  )
+} else {
+  console.error('Root element not found')
+}
